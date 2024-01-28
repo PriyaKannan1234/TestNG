cd5bb7580498d8d11965168321225a61bb43d7af
PriyaKannan1234 <priyakanv@gmail.com>
2024-01-28 14:27:18 -0600

    commit

---
+++ src/test/java/variousConcepts/CrmTest.java
@@ -0,0 +1,62 @@
+package variousConcepts;
+
+import java.sql.Driver;
+import java.util.Random;
+
+import org.testng.annotations.Test;
+
+public class CrmTest {
+
+	
+	
+	
+//	 Don't forget to do REFRESH whenever it needs
+	
+	
+	public void readConfig() {
+	
+//		 InputStream // BufferReader // FileReader // Scanner
+
+		@Test
+		public void testAlert() {
+		Driver
+		
+		}
+	
+		// 0 to 9999
+		
+		Random rnd = new Random();
+		int randomNum = rnd.nextInt();
+		
+		generateRandomNum(9999);
+		
+		selectFromDropdown(Driver.            )
+		
+		Select sell = new sElect(driver.     )
+		driver.find
+		org.openqa.selenium.support.ui.Select sel new Select(Driver.class                        ));
+		sel.selectByVisibleText(company);
+		driver.Ele
+		Driver.Ele
+		
+		Select sell = new Select ()
+	}	
+		
+	private void generateRandomNum(int boundNum)	{
+		Random rnd = new Random();
+		int randomNum = rnd.nextInt(boundNum);
+		return randomNum;
+		
+		
+		
+		
+		// 
+		
+		
+		
+		
+		
+		}	
+		
+   }
+
